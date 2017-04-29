5f3e8d15d57a4d3c33a7b172a1c1b55480b933fd
Joel Ward <jmward@gmail.com>
2017-04-28 19:20:51 -0700

    try me

---
+++ sample.asm
@@ -0,0 +1,24 @@
+	this is sample asm
+
+	here's some more
+
+Hello:
+OOh:	ok i see how this works
+
+
+	Q "wert"
+	W 123
+	X +23
+	Y -23
+	Z $we
+	G ^dfg
+
+	# sdfg
+	;; sdfgdsfg
+	;; dsfgdfgdfg
+
+	# wut
+
+	:	sdfg
+
+	
