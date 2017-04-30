ba24183781278920b4583b6338c5f548451ee650
Joel Ward <jmward@gmail.com>
2017-04-30 16:16:27 -0700

    add macros

--- ae.asm
+++ ae.asm
@@ -1,92 +1,67 @@
-	global _main
-	extern _initscr, _endwin, _getch, _addstr
-	extern _noecho, _raw, _nonl
-	extern _exit, _malloc, _bzero
-	extern _atoi, _printf, _printw
-	default rel
-	section .text
+%include "lib.inc"	
+
+extern _initscr, _endwin, _getch, _addstr
+extern _noecho, _raw, _nonl
+extern _exit, _malloc, _bzero
+extern _atoi, _printf, _printw
+
 init:
-	push	rbp
-
-	pop	rax
-	pop	rbx
-	push	rdi
-	push	rsi
-	push	rdx
-	push	rcx
-	push	r8
-	push	r9
-	push	rbx
-	push	rax
-
-	call	_initscr
+	start "x"
+	
+	callf	_initscr
 	
 	mov	[scr], rax
-	call	_noecho
+	callf	_noecho
 	
-	call	_raw
+	callf	_raw
 	
-	call	_nonl
+	callf	_nonl
 
 	lea	rdi,	[hello]
-	call	_addstr
+	callf	_addstr
 
-	pop	rcx
-	pop	rdx
-	pop	r9
-	pop	r8
-	add	sp,	16
-	pop	rsi
-	pop	rdi
-	push	rdx
-	push	rcx
+	finish "y"
 
-	pop	rbp
-	ret
 break:
-	push	rbp
-	call	_endwin
+	start "x"
+	callf	_endwin
 	lea	rdi,	[goodbye]
-	mov	rax,	0
-	call	_printf
+	callf	_printf
 	mov	rdi,	0
-	call	_exit
+	callf	_exit
+	finish "y"
+
 process:
-	push	rbp
-	mov	rbp,	rsp
-	sub	sp, 16
+	start "x"
 
 	mov	rdx,	[c]
 	mov	rsi,	[c]
 	lea	rdi,	[fmt]
 	mov	ax,	0
-	call	_printw
+	callf	_printw
 
-	add	sp,	16
-	pop	rbp
-	ret
+	finish "y"
 
 _main:
-	push	rbp
-	mov	rbp,	rsp
+	start "x"
 
-	call	init
+	callf	init
 loop:
-	call	_getch
+	callf	_getch
 	mov	[c],	rax
 
 	mov	rdi,	rax
-	call	process
+	callf	process
 
-	cmp	[c],	byte 113
-	je	done
-
-	cmp	[c],	byte 65
-	jne	loop
+	cmp2 [c], byte 113, je, done
+	cmp2 [c], byte 65, je, done
+	
+	jmp	loop
 done:
-	call	break
+	callf	break
+	finish "y"
 
-	section  .data
+section  .data
 scr:	dq      0
 c:	dq      0
 count:	dq      0
@@ -96,4 +71,4 @@ goodbye:db	">> Goodbye!", 10, 0
 format: db      "%g", 10, 0
 fmt:	db      "{%d/%c}", 0
 error:	db      "There are no command line"
-	db " arguments to average", 10, 0
+	db	" arguments to average", 10, 0
